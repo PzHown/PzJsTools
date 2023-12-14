c422ebe1a2f061b133d045f9e3b3a269f6e15e50
PzHown <1263097055@qq.com>
2023-12-14 15:47:16 +0800

    add mounted

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import sleep from './Tools/timeTools.js'
 import creatADiv from './Tools/domTools.js'
+const mounted = () => {
+    console.log("PzJsTools.js加载成功！")
+}
 export default {
     sleep,
-    creatADiv
+    creatADiv,
+    mounted
 }
